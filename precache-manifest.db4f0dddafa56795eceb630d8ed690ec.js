2801a262551fbe1d0219924c0c04e522b7309308
vipin978 <54683308+vipin978@users.noreply.github.com>
2020-07-07 00:04:00 +0530

    Updates

---
+++ precache-manifest.db4f0dddafa56795eceb630d8ed690ec.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a9a7ed6ffcdbe2865dd6881aab65d78",
+    "url": "/IG-Img-Filter/index.html"
+  },
+  {
+    "revision": "d76348aec867d733458d",
+    "url": "/IG-Img-Filter/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "f948b491357011d314d7",
+    "url": "/IG-Img-Filter/static/css/main.15237c91.chunk.css"
+  },
+  {
+    "revision": "d76348aec867d733458d",
+    "url": "/IG-Img-Filter/static/js/2.3ff150e3.chunk.js"
+  },
+  {
+    "revision": "c97fb91e7dd1c7b19ae67fb152a2e6b2",
+    "url": "/IG-Img-Filter/static/js/2.3ff150e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f948b491357011d314d7",
+    "url": "/IG-Img-Filter/static/js/main.f1b546dd.chunk.js"
+  },
+  {
+    "revision": "b58f10a4e92bdc29b7f6",
+    "url": "/IG-Img-Filter/static/js/runtime-main.1732590a.js"
+  },
+  {
+    "revision": "803e367d7cb44614f782a40f97a0ab14",
+    "url": "/IG-Img-Filter/static/media/badOne.803e367d.jpeg"
+  },
+  {
+    "revision": "dca2961968c97d2983db0c68cd062804",
+    "url": "/IG-Img-Filter/static/media/brain.dca29619.svg"
+  }
+]);
